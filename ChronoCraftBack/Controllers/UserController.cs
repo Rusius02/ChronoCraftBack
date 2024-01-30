15ef5ef107e2e40ea5574b4ef6af3e65f78aa1e2
Rusius02 <adrien.stievenart@live.be>
2024-01-30 23:16:37 +0100

    Create endpoint create user

---
+++ ChronoCraftBack/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Application.UseCases.Chrono.dto;
+using Application.UseCases.User;
+using Application.UseCases.User.dto;
+using Infrastructure.SqlServer.Repository.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/Users")]
+    public class UserController : ControllerBase
+    {
+        private readonly UseCaseCreateUser _useCaseCreateUser;
+
+        public UserController(UseCaseCreateUser useCaseCreateUser, IJwtAuthentificationManager jwtAuthentificationManager)
+        {
+            _useCaseCreateUser = useCaseCreateUser;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public ActionResult<OutputDtoCreateUser> Create([FromBody] InputDtoCreateUser user)
+        {
+            /*We call the Execute method of our UseCase and give it a Dto.
+             And it will return an OutputDto of Comment.
+            And we return the code 201 to notify that the request has been made*/
+            return StatusCode(201, _useCaseCreateUser.Execute(user));
+        }
+    }
+}

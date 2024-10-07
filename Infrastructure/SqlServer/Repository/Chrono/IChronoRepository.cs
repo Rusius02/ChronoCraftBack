f187722de0bcaa3b5c89a62ddbc5f7a056b0cc8b
Rusius02 <93829224+Rusius02@users.noreply.github.com>
2024-10-07 15:38:55 +0200

    Create interface repository for Chrono

---
+++ Infrastructure/SqlServer/Repository/Chrono/IChronoRepository.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.SqlServer.Repository.Chrono
+{
+    public interface IChronoRepository
+    {
+        Domain.Chrono Create(Domain.Chrono chrono);
+
+        List<Domain.Chrono> GetAll();
+
+        Domain.Chrono GetUser(Domain.Chrono chronos);
+
+        bool Delete(Domain.Chrono chronos);
+
+        bool Update(Domain.Chrono chronos);
+    }
+}

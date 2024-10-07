3f59921f50dec8e511741de6f8e067e12eaa90b7
Rusius02 <93829224+Rusius02@users.noreply.github.com>
2024-10-07 17:00:42 +0200

    Create ChronoFactory

---
+++ Infrastructure/SqlServer/Repository/Chrono/ChronoFactory.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure.SqlServer.Repository.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.SqlServer.Repository.Chrono
+{
+    public class ChronoFactory
+    {
+        public Domain.Chrono CreateFromSqlReader(SqlDataReader reader)
+        {
+
+            return new Domain.Chrono()
+            {
+                Id = reader.GetInt32(reader.GetOrdinal(ChronoRepository.ColId)),
+                TimeInSecond = reader.GetDouble(reader.GetOrdinal(ChronoRepository.ColTimeInSeconds))
+            };
+        }
+    }
+}

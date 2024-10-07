caf5158d182c33e8a20ee9cc84f34562783bfafe
Rusius02 <93829224+Rusius02@users.noreply.github.com>
2024-10-07 15:53:50 +0200

    Create the ChronoRequest file

---
+++ Infrastructure/SqlServer/Repository/Chrono/ChronoRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.SqlServer.Repository.Chrono
+{
+    public partial class ChronoRepository
+    {
+        public const string TableName = "chronos",
+            ColId = "id",
+            ColTimeInSeconds = "timeInSeconds";
+
+        public static readonly string ReqCreate = $@"
+        INSERT INTO {TableName}({ColTimeInSeconds})
+        OUTPUT INSERTED.{ColId}
+        VALUES(@{ColTimeInSeconds})";
+
+        public static readonly string ReqGetAll = $@"
+        SELECT * FROM {TableName}";
+
+        public static readonly string ReqGetById = $@"
+        SELECT * FROM {TableName}
+        WHERE {ColId} = @{ColId}";
+
+        public static readonly string ReqDelete = $@"
+            DELETE FROM {TableName} WHERE {ColId} = @{ColId}";
+
+        public static readonly string ReqUpdate = $@"
+            UPDATE {TableName}
+            SET {ColTimeInSeconds} = @{ColTimeInSeconds}
+            WHERE {ColId} = @{ColId}";
+
+    }
+}

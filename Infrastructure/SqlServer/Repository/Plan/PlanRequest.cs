4c67201749ce5bb870c92df2838155a4d313940a
Rusius02 <93829224+Rusius02@users.noreply.github.com>
2024-10-08 22:46:19 +0200

    PlanRequest file creation

---
+++ Infrastructure/SqlServer/Repository/Plan/PlanRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.SqlServer.Repository.Plan
+{
+    public partial class PlanRepository
+    {
+        public const string TableName = "plans",
+           ColId = "id",
+           ColTimeInSeconds = "timeInSeconds";
+
+        public static readonly string ReqCreate = $@"
+        INSERT INTO {TableName}({ColTimeInSeconds})
+        OUTPUT INSERTED.{ColId}
+        VALUES(@{ColTimeInSeconds})";
+
+        public static readonly string ReqGetAll = $@"
+        SELECT * FROM {TableName}";
+
+        public static readonly string ReqGetById = $@"
+        SELECT * FROM {TableName}
+        WHERE {ColId} = @{ColId}";
+
+        public static readonly string ReqDelete = $@"
+            DELETE FROM {TableName} WHERE {ColId} = @{ColId}";
+
+        public static readonly string ReqUpdate = $@"
+            UPDATE {TableName}
+            SET {ColTimeInSeconds} = @{ColTimeInSeconds}
+            WHERE {ColId} = @{ColId}";
+    }
+}
